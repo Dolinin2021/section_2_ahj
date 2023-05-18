c4fe07fc6cedadb6909cafe94da9ac3cba4a9b35
Dolinin2021 <programmistdolinin@mail.ru>
2023-05-18 15:40:33 +0600

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,24 @@
 (() => {
   let playing = true;
   let activeHole = 0;
-  let holeGame = document.querySelector('.hole-game');
+  let holeGame = document.querySelector(".hole-game");
   let child = holeGame.children;
 
-  holeGame.onclick = function(event) {
+  holeGame.onclick = function (event) {
     if (event.target.classList.contains("hole_has-mole")) {
       event.target.classList.remove("hole_has-mole");
       next.clearTimeout;
     }
-  }
+  };
 
-  next = () =>
+  let next = () =>
     setTimeout(() => {
       if (!playing) {
         return;
       }
       child[activeHole].className = "hole";
       activeHole = Math.floor(1 + Math.random() * 7);
-      child[activeHole].className = "hole hole_has-mole"
+      child[activeHole].className = "hole hole_has-mole";
       next();
     }, 800);
   next();
