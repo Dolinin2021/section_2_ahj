44a5b4e7a2ab293b62f6626d26146107c29b18d8
Dolinin2021 <programmistdolinin@mail.ru>
2023-05-18 15:28:52 +0600

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,54 +1,25 @@
-const dead = document.getElementById("dead");
-const lost = document.getElementById("lost");
-
-let countDead = 0;
-let countLost = 0;
-
-for (let index = 1; index < 10; index++) {
-  const getHole = (index) => document.getElementById(`hole${index}`);
-  let data = getHole(index);
-
-  data.onclick = () => {
-    if (data.classList.contains("hole_has-mole")) {
-      dead.textContent = Number(dead.textContent) + 1;
-      console.log(data);
-      countDead++;
-    } else {
-      lost.textContent = Number(lost.textContent) + 1;
-      countLost++;
-    }
-    if (countDead === 10) {
-      alert("Вы победили!");
-      countDead = 0;
-      countLost = 0;
-      window.location.reload();
-    } else if (countLost === 5) {
-      alert("Вы проиграли!");
-      countDead = 0;
-      countLost = 0;
-      window.location.reload();
-    }
-  };
-}
-
 (() => {
-  let playing = true,
-    activeHole = 1;
+  let playing = true;
+  let activeHole = 0;
+  let holeGame = document.querySelector('.hole-game');
+  let child = holeGame.children;
 
-  const stop = () => (playing = true),
-    getHole = (index) => document.getElementById(`hole${index}`),
-    deactivateHole = (index) => (getHole(index).className = "hole"),
-    activateHole = (index) => (getHole(index).className = "hole hole_has-mole"),
-    next = () =>
-      setTimeout(() => {
-        if (!playing) {
-          return;
-        }
-        deactivateHole(activeHole);
-        activeHole = Math.floor(1 + Math.random() * 9);
-        activateHole(activeHole);
-        next();
-      }, 800);
+  holeGame.onclick = function(event) {
+    if (event.target.classList.contains("hole_has-mole")) {
+      event.target.classList.remove("hole_has-mole");
+      next.clearTimeout;
+    }
+  }
 
+  next = () =>
+    setTimeout(() => {
+      if (!playing) {
+        return;
+      }
+      child[activeHole].className = "hole";
+      activeHole = Math.floor(1 + Math.random() * 7);
+      child[activeHole].className = "hole hole_has-mole"
+      next();
+    }, 800);
   next();
 })();
